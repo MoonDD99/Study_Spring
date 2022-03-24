2b7cf02c83a6ab97dc204c602f81cf1dc4dbaee5
MoonDD99 <mjh991016@naver.com>
2022-03-25 01:55:09 +0900

    implement hellocontroller

---
+++ src/main/java/com/study_spring/studyspring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.study_spring.studyspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "안뇽");
+        return "hello";
+    }
+}
